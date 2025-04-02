3d540f09fb8ca91fb0c18926f1b2c41d0783066d
PaPerso <pa.raclius2@gmail.com>
2025-04-02 20:56:24 +0200

    il n'y avait pas besoin de tout ca

--- assets/js/variables.js
+++ assets/js/variables.js
@@ -1,15 +1,9 @@
 const site_name = "BNC-website";
-
-const index_url = site_name.concat("/index.html");
-const produits_url = site_name.concat("/pages/produits.html");
-const localisation_url = site_name.concat("/pages/localisation.html");
-const commande_url = site_name.concat("/pages/commande.html");
-const contact_url = site_name.concat("/pages/contact.html");
-
 const LINKS = {
-    index: index_url,
-    produits: produits_url,
-    localisation: localisation_url,
-    commande: commande_url,
-    contact: contact_url
-};
+    index: "/index.html",
+    produits: "/pages/produits.html",
+    localisation: "/pages/localisation.html",
+    commande: "/pages/commande.html",
+    contact: "/pages/contact.html"
+  };
+  
