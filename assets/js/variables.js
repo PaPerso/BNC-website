587c529cba36ee17053fe47c5ebf20aeb256b8cf
PaPerso <pa.raclius2@gmail.com>
2025-04-02 21:00:36 +0200

    je vais pas y arriver

--- assets/js/variables.js
+++ assets/js/variables.js
@@ -1,9 +1,9 @@
 const site_name = "BNC-website";
 const LINKS = {
-    index: "/index.html",
-    produits: "/pages/produits.html",
-    localisation: "/pages/localisation.html",
-    commande: "/pages/commande.html",
-    contact: "/pages/contact.html"
+    index: "index.html",
+    produits: "pages/produits.html",
+    localisation: "pages/localisation.html",
+    commande: "pages/commande.html",
+    contact: "pages/contact.html"
   };
   
