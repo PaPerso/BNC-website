edd2586687bb85a7f20e61f501ac58ee4c2cb934
P-A Raclius <pa.raclius@gmail.com>
2025-04-02 20:05:01 +0200

    Adding JS for links between pages

---
+++ assets/js/variables.js
@@ -0,0 +1,8 @@
+const LINKS = {
+    index: "/index.html",
+    produits: "/pages/produits.html",
+    localisation: "/pages/localisation.html",
+    commande: "/pages/commande.html",
+    contact: "/pages/contact.html"
+  };
+  
