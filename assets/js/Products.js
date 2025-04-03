9640edd210374a97f76aeaa82dc76fcea282d87e
PaPerso <pa.raclius2@gmail.com>
2025-04-03 20:00:59 +0200

    Adding generative products list

---
+++ assets/js/Products.js
@@ -0,0 +1,26 @@
+const PRODUCTS=[
+    {
+        nom: "Banana Bread",
+        description: "Moelleux et parfumé à la banane, parfait pour le brunch."
+    },
+    {
+        nom: "Pancakes",
+        description: "Légers et dorés, accompagnés de sirop d'érable."
+    },
+    {
+        nom: "Morning Muffins",
+        description: "McMuffins faits maison avec des produits végés."
+    },
+    {
+        nom: "Apple Rings",
+        description: "Pancakes garnis d'une fine tranche de pomme fondante."
+    },
+    {
+        nom: "Brownies",
+        description: "Gourmands, riches en chocolat, texture fondante."
+    },
+    {
+        nom: "Croc'choc",
+        description: "Petits pains briochés garnis de pépites de chocolat."
+    }
+];
