1799d5b858a2a1de4254bc32d4948ed2b36b3acb
PaPerso <pa.raclius2@gmail.com>
2025-04-10 17:52:55 +0200

    Adding pictures

--- assets/js/products.js
+++ assets/js/products.js
@@ -1,26 +1,32 @@
 const PRODUCTS=[
     {
         name: "Banana Bread",
-        description: "Moelleux et parfumé à la banane, parfait pour le brunch."
+        description: "Moelleux et parfumé à la banane, parfait pour le brunch.",
+        pic: ""
     },
     {
         name: "Pancakes",
-        description: "Légers et dorés, accompagnés de sirop d'érable."
+        description: "Légers et dorés, accompagnés de sirop d'érable.",
+        pic: ""
     },
     {
         name: "Morning Muffins",
-        description: "McMuffins faits maison avec des produits végés."
+        description: "McMuffins faits maison avec des produits végés.",
+        pic: ""
     },
     {
         name: "Apple Rings",
-        description: "Pancakes garnis d'une fine tranche de pomme fondante."
+        description: "Pancakes garnis d'une fine tranche de pomme fondante.",
+        pic: ""
     },
     {
         name: "Brownies",
-        description: "Gourmands, riches en chocolat, texture fondante."
+        description: "Gourmands, riches en chocolat, texture fondante.",
+        pic: ""
     },
     {
         name: "Croc'choc",
-        description: "Petits pains briochés garnis de pépites de chocolat."
+        description: "Petits pains briochés garnis de pépites de chocolat.",
+        pic: ""
     }
 ];
